cd85b5a0d9a9471adaddbdd64833abf2f4654074
jamesackies <jamesackies8@gmail.com>
2023-01-13 17:46:39 -0500

    commit tree f131a226bd6604378e32de72429cbe69b395dde7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts)
+
+const anArrayOfStringNames = ["bat", "cat", "mat"] 
+
+function writeCards(anArrayOfStringNames, eventName){
+    const newArray= []
+    for (let i = 0; i < anArrayOfStringNames.length; i++) {
+newArray.push(`Thank you, ${anArrayOfStringNames[i] }, for the wonderful ${eventName} gift!`)
+    } 
+    return newArray
+}
+function countDown() {
+let countDown = 10
+while (countDown >= 0){
+    console.log(countDown--)
+}
+}
